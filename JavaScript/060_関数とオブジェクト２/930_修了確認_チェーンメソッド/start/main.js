fa5554ace1584ef5cd6523536e02ef671b319642
Hero <hero.not.found.404@gmail.com>
2024-04-05 23:41:05 +0900

    2024/04/05復習分

--- JavaScript/060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
+++ JavaScript/060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
@@ -2,21 +2,21 @@
  * 問題：
  * 電卓の入力と同じような挙動をするチェーンメソッド
  * を作成してみましょう。
- * 
+ *
  * 例えば、次のように使用し、結果が表示
  * されるようにします。
- * 
- * 
+ *
+ *
  * 例１）
  * const calc = new Calculator();
- * 
+ *
  * calc.set(10)
  * .minus()
  * .set(3) -> '7'を出力(10 - 3)
- * 
+ *
  * 例２）
  * const calc = new Calculator();
- * 
+ *
  * calc.set(10)
  * 	.minus()
  * 	.set(3) -> '7'を出力
@@ -24,14 +24,33 @@
  * 	.set(6) -> '42'を出力（10 - 3) * 6
  */
 
-const calc = new Calculator();
+class Calculator {
+  constructor() {
+    this.num = null;
+  }
+  set(num) {
+    console.log(this);
+    if (this.num === null) {
+      this.num = num;
+    } else {
+      this._operator(this.num, num);
+    }
+    return this;
+  }
+  minus() {
+    console.log(this);
+    this._operator = function (val1, val2) {
+      const result = val1 - val2;
+      this._equal(result);
+    };
+    return this;
+  }
 
-calc.set(10)
-	.minus()
-	.set(3)
-	.mutiply()
-	.set(6)
-	.divide()
-	.set(2)
-	.plus()
-	.set(2)
+  _equal(result) {
+    this.num = result;
+    console.log(result);
+  }
+}
+
+const calc = new Calculator();
+calc.set(10).minus().set(3).mutiply().set(6).divide().set(2).plus().set(2);
