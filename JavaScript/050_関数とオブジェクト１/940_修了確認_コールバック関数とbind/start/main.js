77243695912fc752f60fd3d4847e4890dd061124
Hero <hero.not.found.404@gmail.com>
2024-04-05 23:40:46 +0900

    2024/04/05復習分

--- JavaScript/050_関数とオブジェクト１/940_修了確認_コールバック関数とbind/start/main.js
+++ JavaScript/050_関数とオブジェクト１/940_修了確認_コールバック関数とbind/start/main.js
@@ -4,33 +4,37 @@
  * １秒後にconsole.logまたはalertで計算式が表示される
  * ようにcalcFactory関数内の実装を変更してみてください。
  */
-function calcFactory(val, callback) {
-    return {
-        plus: function(target) {
-            const newVal = val + target;
-            callback(`${val} + ${target} = ${newVal}`);
-            val = newVal;
-        },
-        minus: function(target) {
-            const newVal = val - target;
-            callback(`${val} - ${target} = ${newVal}`);
-            val = newVal;
-        },
-        multiply: function(target) {
-            const newVal = val * target;
-            callback(`${val} x ${target} = ${newVal}`);
-            val = newVal;
-        },
-        divide: function(target) {
-            const newVal = val / target;
-            callback(`${val} / ${target} = ${newVal}`);
-            val = newVal;
-        }
-    };
-}
 
+function calcFactory(val, ck) {
+  function ckAfter1s(str) {
+    setTimeout(ck.bind(null, str), 1000);
+  }
+
+  return {
+    plus: function (target) {
+      const newVal = val + target;
+      ckAfter1s(`${val} + ${target} = ${newVal}`);
+      val = newVal;
+    },
+    minus: function (target) {
+      const newVal = val - target;
+      ckAfter1s(`${val} - ${target} = ${newVal}`);
+      val = newVal;
+    },
+    multiply: function (target) {
+      const newVal = val * target;
+      ckAfter1s(`${val} x ${target} = ${newVal}`);
+      val = newVal;
+    },
+    divide: function (target) {
+      const newVal = val / target;
+      ckAfter1s(`${val} / ${target} = ${newVal}`);
+      val = newVal;
+    },
+  };
+}
 const calc = calcFactory(10, console.log);
-calc.plus(5); 
-calc.minus(3); 
+calc.plus(5);
+calc.minus(3);
 calc.multiply(3);
 calc.divide(2);
